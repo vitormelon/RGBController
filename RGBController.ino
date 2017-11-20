d7738ce20a34b7918dee68a060ce267ba24d721a
Vitor Melon <vitormelon@gmail.com>
2017-11-19 23:45:38 -0200

    Added the functionality of reading the potentiometer and changing color intensity based on the potentiometer position

--- RGBController.ino
+++ RGBController.ino
@@ -7,18 +7,22 @@
  * ADD potentiometer and do the map thing
  * add to github
  */
- 
+
 
 /* ==== Defines ==== */
-
-#define SERIAL_BAUD 115200
-#define LED_RED   2
-#define LED_GREEN 3
-#define LED_BLUE  4
-#define BUTTON    5
-#define MENU_MAX  3
-#define ANALOG    A1
-#define TIME_OUT  10*1000   //x * 1000 = convertion from miliseconds to seconds
+#define SERIAL_BAUD     115200
+#define LED_RED         2 //Red LED port
+#define LED_GREEN       3 //Green LED port
+#define LED_BLUE        4 //Blue LED port
+#define BUTTON          5 //Push-button port
+#define MENU_MAX        3 //MAX menu states ( + the 0 zero)
+#define POTENTIOMETER   A0//Potentiometer port
+#define TIME_OUT        10*1000   //x * 1000 = convertion from miliseconds to seconds
+#define MAX_VARIATION   5 //max variation on the potentiometer read to start editing.
+#define RED             1 //array position of each color
+#define GREEN           2
+#define BLUE            3
+#define GENERAL         0
 
 /* ==== END Defines ==== */
 
@@ -33,7 +37,9 @@
  */
 int menuState = 0;
 long timeout = 0;
-int redIntesity = 1023, greenIntensity = 1023, blueIntesity = 1023; //colors itensity begns with "white"
+int ledValue[4] = {1023, 1023, 1023, 1023}; //colors itensity begns with max, generating "white" color
+int potentiometerValue = 0;
+boolean isEditing = false;
 /* ==== END Global Variables ==== */
 
 
@@ -47,34 +53,91 @@ void setup() {
 
 void loop() {
   checkButtonPress();
+  checkPotentiometerChange();
   menu();
 }
 /* ==== Functions ==== */
 void menu(){
   switch(menuState){
-    case 0:
-      //general
-      break;
-    case 1:
-      digitalWrite(LED_RED, HIGH);//red
-      break;
-    case 2:
-      digitalWrite(LED_GREEN, HIGH);//green
-      break;
-    case 3:
-      digitalWrite(LED_BLUE, HIGH);//blue
-      break;
+  case 0:
+    menuAction(0, GENERAL);
+    break;
+  case RED:
+    menuAction(LED_RED, RED);//red
+    break;
+  case GREEN:
+    menuAction(LED_GREEN, GREEN);//green
+    break;
+  case BLUE:
+    menuAction(LED_BLUE, BLUE);//blue
+    break;
   }
 }
 
+void menuAction(int monitorPort, int color){
+  printStatusOnSerial();
+  digitalWrite(monitorPort, HIGH);
+  if(isEditing){
+    ledValue[color] = readPotentiometer();
+  }
+}
+
+void printStatusOnSerial(){
+  Serial.print("Menu: ");
+  Serial.print(menuState);
+  
+  Serial.print(" IsEditing: ");
+  Serial.print(isEditing);
+  
+  Serial.print(" Potentiometer: ");
+  Serial.print(readPotentiometer());
+  
+  Serial.print(" General: ");
+  Serial.print(ledValue[GENERAL]);
+  
+  Serial.print(" Red: ");
+  Serial.print(ledValue[RED]);
+  
+  Serial.print(" Green: ");
+  Serial.print(ledValue[GREEN]);
+  
+  Serial.print(" Blue: ");
+  Serial.println(ledValue[BLUE]);
+  
+}
+
 void checkButtonPress(){
+  int menuStateBefore = menuState;
   if(digitalRead(BUTTON) == HIGH){
     menuState ++;
     delay(250);
     ledToLow();
   }
   if(menuState > MENU_MAX) menuState = 0;
-    
+  if(menuState != menuStateBefore) onMenuStateChange();
+}
+
+void checkPotentiometerChange(){
+  if(!isEditing){
+    int valueNow = readPotentiometer();
+    if(getPotentiometerVariation(valueNow) > MAX_VARIATION){
+      isEditing = true;
+    }
+  }
+}
+
+int getPotentiometerVariation(int valueNow){
+  return abs(valueNow-potentiometerValue);
+}
+
+void onMenuStateChange(){
+  isEditing = false;
+  potentiometerValue = readPotentiometer();
+}
+
+int readPotentiometer(){
+  int analogicValue = analogRead(POTENTIOMETER);
+  return map(analogicValue, 0, 1023, 0, 255);  
 }
 
 void ledToLow(){  
@@ -98,4 +161,5 @@ void blinkLed(int led, int time){
   delay(time);
   digitalWrite(led, LOW);
 }
+
 
